fda2bc07bc9a52c3cf1c9012139b5d0f84616c77
JorgeOlanSantos <89498253+JorgeOlanSantos@users.noreply.github.com>
2022-07-25 05:17:41 -0700

    Added Customer class [INCOMPLETE]
    Added incomplete Customer class, last edited early 2022-07-25

---
+++ Customer.java
@@ -0,0 +1,77 @@
+import java.util.Vector;
+
+public class Customer extends User { // Class for banking system customer
+	// Inherited attributes
+	String name;                    // Customer name
+	String ID;                      // Customer ID number
+	Vector<ActionType> permissions; // Customer permissions
+	
+	// Class attributes
+	String address;   // Customer's address(?)
+	short pin;        // Customer pin(?)
+	boolean loggedIn; // Whether customer is logged into ATM/bank system(?)
+	
+	// Constructors
+	
+	public Customer(Login login, String name, short pin) { // Parameterized constructor
+		
+		// Do something with Login object (for logging in as customer?)
+		setName(name); // Set name ? 
+		setPin(pin);   // Set pin ?
+	}
+	
+	public Customer(String ID, String password, String name, short pin) { // Parameterized constructor
+		
+		setID(ID);     // Set ID ? // Use in Login object?
+		                           // Use password in Login object?
+		
+		setName(name); // Set name ? 
+		setPin(pin);   // Set pin ?
+	}
+	
+	// Inherited methods
+	
+	public String getName() { // get customer name
+		return name;
+	}
+	
+	public String getID() { // get customer ID
+		return ID;
+	}
+	
+	public void setName(String name) { // set customer name
+		this.name = name;
+	}
+	
+	public void setID(String ID) { // set customer ID
+		this.ID = ID;
+	}
+	
+	// Class methods
+	
+	public String getAddress() { // get customer address
+		return address;
+	}
+	
+	public short getPin() { // get customer pin
+		return pin;
+	}
+	
+	public boolean getloggedIn() { // get customer loggedIn
+		return loggedIn;
+	}
+	
+	public void setAddress(String address) { // set customer address
+		this.address = address;
+	}
+	
+	public void setPin(short pin) { // set customer pin
+		this.pin = pin;
+	}
+	
+	public void setLoggedIn(boolean loggedIn) { // set customer loggedIn
+		this.loggedIn = loggedIn;
+	}
+	
+	
+}
